ed7e5f8cfa7d5104a707cff0e4099b9332cad891
wookkl <wjddnr315@gmail.com>
2021-02-14 15:11:47 +0900

    Add css and pagination feature in home view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from communities.models import Community
+from posts.models import Post
 
 
 @require_http_methods(["GET"])
 def home_view(request):
-    return render(request, "home.html")
+    """Home view"""
+    posts = Post.objects.all()
+    page = request.GET.get("page", 1)
+    paginator = Paginator(posts, 20)
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        paginated_posts = paginator.page(paginator.num_pages)
+
+    community_obj = Community.objects.all().order_by("-pk")[:10]
+
+    return render(
+        request,
+        "home.html",
+        {"communities": community_obj, "posts": paginated_posts},
+    )
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin definition"""
+
+    list_display = (
+        "writer",
+        "community",
+        "content",
+        "get_thumbnail",
+    )
+
+    def get_thumbnail(self, instance):
+        if instance.photo:
+            return mark_safe(
+                f"<img src={instance.photo.url} style='width:40px; height:40px;'/> "
+            )
+        else:
+            return None
+
+    get_thumbnail.short_description = "thumbnail"
