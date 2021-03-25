704fcf515c84388dfa556eea3750773c90ff57c5
wookkl <wjddnr315@gmail.com>
2021-03-25 19:20:46 +0900

    Add staticfiles_dirs

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,6 +121,9 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+]
 # Media files
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
