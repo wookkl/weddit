e835e22f59df26b55b7e9b37aa9e9a254cf7d922
wookkl <wjddnr315@gmail.com>
2021-03-25 12:56:15 +0900

    Fix db configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,11 +4,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ["weddit.eba-be3gdpu7.ap-northeast-2.elasticbeanstalk.com"]
 
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["RDS_DB_NAME"],
         "USER": os.environ["RDS_USERNAME"],
         "PASSWORD": os.environ["RDS_PASSWORD"],
         "HOST": os.environ["RDS_HOSTNAME"],
