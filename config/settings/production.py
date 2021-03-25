e2a7e426ef957b18cd21934901d2228cd67ea1a2
wookkl <wjddnr315@gmail.com>
2021-03-25 12:05:51 +0900

    Configure elastic beanstalk #2

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,10 +9,10 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("RDS_HOST_NAME"),
+        "PORT": os.environ.get("RDS_PORT"),
         "NAME": os.environ.get("RDS_DB_NAME"),
         "USER": os.environ.get("RDS_USERNAME"),
         "PASSWORD": os.environ.get("RDS_PASSWORD"),
-        "HOST": os.environ.get("RDS_HOST_NAME"),
-        "PORT": os.environ.get("RDS_PORT"),
     }
 }
