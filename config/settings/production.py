f49cd8519358e8d44955895ef8323d6cf964e48e
wookkl <wjddnr315@gmail.com>
2021-03-25 12:38:10 +0900

    Fix db configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,10 +9,9 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("RDS_HOST_NAME"),
-        "PORT": os.environ.get("RDS_PORT"),
-        "NAME": os.environ.get("RDS_DB_NAME"),
-        "USER": os.environ.get("RDS_USERNAME"),
-        "PASSWORD": os.environ.get("RDS_PASSWORD"),
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
     }
 }
